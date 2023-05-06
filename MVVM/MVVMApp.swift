d7922174e30047b7cfa349b9ecbfa595dc946130
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-05-06 21:55:38 +0600

    project init

---
+++ MVVM/MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMApp.swift
+//  MVVM
+//
+//  Created by Shah Md Imran Hossain on 6/5/23.
+//
+
+import SwiftUI
+
+@main
+struct MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
